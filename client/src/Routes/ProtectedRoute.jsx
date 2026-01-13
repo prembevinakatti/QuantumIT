f8b8e271fb66643a5d4ef701e269ec2c29a7a7b1
OnkarBevinakatti <onkarbevinakatti09@gmail.com>
2026-01-13 23:07:28 +0530

    ..

--- client/src/Routes/ProtectedRoute.jsx
+++ client/src/Routes/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import toast from "react-hot-toast";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
   if (!token) {
+    // toast.error("You are not logged in"); this appears two times because of the react strict mode
     return <Navigate to="/" replace />;
   }
 
